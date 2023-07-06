a6c2c01249010caff0da19dbf269459ce92cfea8
Vlad Tsygankov <tshugankov@mail.ru>
2023-07-07 00:25:46 +0300

    v1

--- conftest.py
+++ conftest.py
@@ -1,15 +1,25 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from data import BASE, LOGIN_PAGE, LOGIN, PASSWORD
+from locators import Locators as L
 
-url = 'https://stellarburgers.nomoreparties.site'
 
 @pytest.fixture
 def driver():
-    browser = webdriver.Chrome()
-    browser.get(url)
-    yield browser
-    browser.quit()
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(BASE)
+    yield driver
+    driver.quit()
 
-class VALID_ACC:
-    email = "vlad_tsygankov_111@mail.ru"
-    password = "123456"
+
+@pytest.fixture()
+def valid_register(driver):
+    driver.get(LOGIN_PAGE)
+    driver.find_element(*L.login_email).send_keys(LOGIN)
+    driver.find_element(*L.password).send_keys(PASSWORD)
+    driver.find_element(*L.login_button).click()
+    WebDriverWait(driver, 2).until(EC.visibility_of_element_located(L.burger_constructor))
+    return driver
